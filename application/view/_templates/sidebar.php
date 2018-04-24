fcf9b76a2ade8c2df228fc443919916e744085a3
henri.silvatj <henri.silvatj@hotmail.com>
2018-04-24 15:06:37 -0300

    Sidebar

--- application/view/_templates/sidebar.php
+++ application/view/_templates/sidebar.php
@@ -7,28 +7,34 @@
             <ul id="sidebarnav">
                 <li class="nav-devider"></li>
                 <li class="nav-label">Home</li>
-                <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Dashboard <span class="label label-rouded label-primary pull-right">2</span></span></a>
+                <li>
+                    <a href="#" aria-expanded="false">
+                        <i class="fa fa-home"></i>
+                        <span class="hide-menu">Geral</span>
+                    </a>
+                </li>
+<!--                <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Dashboard <span class="label label-rouded label-primary pull-right">2</span></span></a>-->
+<!--                    <ul aria-expanded="false" class="collapse">-->
+<!--                        <li><a href="#">Geral </a></li>-->
+<!--                        <li><a href="#">Relatórios </a></li>-->
+<!--                    </ul>-->
+<!--                </li>-->
+                <li class="nav-label">Apps</li>
+                <li> <a class="has-arrow" href="#" aria-expanded="false"><i class="fa fa-product-hunt"></i><span class="hide-menu">Produtos</span></a>
                     <ul aria-expanded="false" class="collapse">
-                        <li><a href="#">Ecommerce </a></li>
-                        <li><a href="#">Analytics </a></li>
+                        <li><a href="#">Relatórios</a></li>
+                        <li><a href="#">Preços</a></li>
+                        <li><a href="#">Mercados</a></li>
                     </ul>
                 </li>
-                <li class="nav-label">Apps</li>
-                <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-envelope"></i><span class="hide-menu">Email</span></a>
+                <li class="nav-label">Recursos</li>
+                <li> <a class="has-arrow" href="#" aria-expanded="false"><i class="fa fa-envelope"></i><span class="hide-menu">Email</span></a>
                     <ul aria-expanded="false" class="collapse">
                         <li><a href="#">Compose</a></li>
                         <li><a href="#">Read</a></li>
                         <li><a href="#">Inbox</a></li>
                     </ul>
                 </li>
-                <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-bar-chart"></i><span class="hide-menu">Charts</span></a>
-                    <ul aria-expanded="false" class="collapse">
-                        <li><a href="#">Flot</a></li>
-                        <li><a href="#">Morris</a></li>
-                        <li><a href="#">ChartJs</a></li>
-                    </ul>
-                </li>
-                <li class="nav-label">Features</li>
             </ul>
         </nav>
     </div>
